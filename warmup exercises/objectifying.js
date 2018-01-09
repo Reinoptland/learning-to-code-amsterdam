427957beb38f5ef370a9fa26487f698917a4709f
Rein Op 't Land <rein@kabisa.nl>
2018-01-09 01:29:54 +0100

    The handheld honeymoon

---
+++ warmup exercises/objectifying.js
@@ -0,0 +1,46 @@
+/*
+  Warmup exercise: Objectifying!
+
+  A big part of programming is to describe real life situations and things
+  in terms of data. This is called 'data modelling'.
+
+  Let's say we're going on vacation. How can we describe what a vacation 'is'?
+  It might contain the following:
+  - The people going on the vacation
+  - The date when the vacation starts
+  - The number of days the vacation will last
+  - The destination
+  - The schedule of travelling to the destination
+  - What we are going to do when we're there
+
+  Now the challenge becomes to make a good translation from these properties
+  to the right 'data-types'
+
+  There are a lot of data-types in the world of programming so we'll start with
+  some simple ones.
+
+  Strings: represents textual data such as names. Denoted bij '' or ""
+  example 'Danny'
+
+  Numbers: represents a numerical value
+  example 2018
+
+  Boolean: true or false
+
+  Array: A collection of data with a specific order. Denoted by []. A comma
+  separates each item in the collection.
+  example: ['Breakfast', 'Lunch', 'Dinner'] (this is an Array of strings)
+
+  Object: A collection of properties with value. Denoted by {}. A comma
+  separates each item in the collection.
+  example: {
+    name: 'Rein',
+    age: 30,
+    alive: true,
+    jobs: ['tutor','teacher', 'programmer'],
+  }
+
+  Today's exercise
+  - You've received some kind of object or situation
+  - You're goin to model it in JSON (javascript object notation)
+*/
